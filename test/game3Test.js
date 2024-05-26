d697925f9588d620ef36ac87eede9fd50a4d9062
choi <caihua90@qq.com>
2024-05-26 11:42:08 +0800

    finish all tests

--- test/game3Test.js
+++ test/game3Test.js
@@ -1,9 +1,9 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
 
-describe('Game3', function () {
+describe("Game3", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game3');
+    const Game = await ethers.getContractFactory("Game3");
     const game = await Game.deploy();
 
     // Hardhat will create 10 accounts for you by default
@@ -18,18 +18,25 @@ describe('Game3', function () {
     return { game, signer };
   }
 
-  it('should be a winner', async function () {
+  it("should be a winner", async function () {
     const { game, signer } = await loadFixture(deployContractAndSetVariables);
-
+    const s2 = signer;
     // you'll need to update the `balances` mapping to win this stage
-
+    const s1 = ethers.provider.getSigner(1);
+    const s3 = ethers.provider.getSigner(2);
     // to call a contract as a signer you can use contract.connect
-    await game.connect(signer).buy({ value: '1' });
+    await game.connect(s2).buy({ value: "3" });
+    await game.connect(s1).buy({ value: "2" });
+    await game.connect(s3).buy({ value: "1" });
 
     // TODO: win expects three arguments
-    await game.win();
+    await game.win(
+      await s1.getAddress(),
+      await s2.getAddress(),
+      await s3.getAddress()
+    );
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
--- test/game5Test.js
+++ test/game5Test.js
@@ -1,21 +1,41 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const {
+  loadFixture,
+  setBalance,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
+const { ethers } = require("hardhat");
+const { utils } = ethers;
 
-describe('Game5', function () {
+describe("Game5", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game5');
+    const Game = await ethers.getContractFactory("Game5");
     const game = await Game.deploy();
 
     return { game };
   }
-  it('should be a winner', async function () {
+  it("should be a winner", async function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
 
     // good luck
-
-    await game.win();
+    const threshold = parseInt(
+      "0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf",
+      16
+    );
+    let sender;
+    let i = 0;
+    while (++i) {
+      const wallet = ethers.Wallet.createRandom();
+      const { address } = await wallet;
+      if (parseInt(address, 16) < threshold) {
+        console.log(i, address);
+        sender = wallet.connect(ethers.provider);
+        await setBalance(address, utils.parseEther("1"));
+        break;
+      }
+    }
+    await game.connect(sender).win();
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
